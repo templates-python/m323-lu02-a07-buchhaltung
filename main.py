e7873f8f231982fc87ef4db1ddd7e93066a3b2b0
Kevin Maurizi <kevin.maurizi@bzz.ch>
2023-08-30 15:23:38 +0200

    done

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-def calculate():
-    """
-    calculates the total
-    :return: None
-    """
-    price = 15.75
-    quantity = 3  # TODO change quantity to 5
-    print(f'Total: {price * quantity}')
-
+# Dein Code kommt hier hin
 
 if __name__ == '__main__':
-    calculate()
+    # Dein Code kommt hier hin
+    pass
--- main_test.py
+++ main_test.py
@@ -1,6 +1,12 @@
 import main
 
-def test(capsys):
-    main.calculate()
-    captured = capsys.readouterr()
-    assert captured.out == "Total: 78.75\n"
+def test_add_transaction():
+    initial_transactions = (('Deposit', 1000), ('Withdrawal', 200))
+    new_transaction = ('Deposit', 500)
+    updated_transactions = main.add_transaction(initial_transactions, new_transaction)
+    assert updated_transactions == (('Deposit', 1000), ('Withdrawal', 200), ('Deposit', 500))
+
+def test_calculate_balance():
+    transactions = (('Deposit', 1000), ('Withdrawal', 200), ('Deposit', 500))
+    balance = main.calculate_balance(transactions)
+    assert balance == 1300
