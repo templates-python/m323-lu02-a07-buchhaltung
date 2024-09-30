3b4b62f14390c19948633e5825b9e818275d8cc4
Kevin Maurizi <kevin.maurizi@bzz.ch>
2024-09-30 09:03:27 +0200

    Ran pyink on branch main

--- main_test.py
+++ main_test.py
@@ -1,10 +1,16 @@
 import main
 
+
 def test_add_transaction():
     initial_transactions = (('Deposit', 1000), ('Withdrawal', 200))
     new_transaction = ('Deposit', 500)
     updated_transactions = main.add_transaction(initial_transactions, new_transaction)
-    assert updated_transactions == (('Deposit', 1000), ('Withdrawal', 200), ('Deposit', 500))
+    assert updated_transactions == (
+        ('Deposit', 1000),
+        ('Withdrawal', 200),
+        ('Deposit', 500),
+    )
+
 
 def test_calculate_balance():
     transactions = (('Deposit', 1000), ('Withdrawal', 200), ('Deposit', 500))
